0ea3f116bb855140c5ffc79c0628d395d8ac4acc
hasnainbkg <73811984+hasnainbkg@users.noreply.github.com>
2021-01-30 02:26:55 +0500

    Update zeroboy.py

--- zeroboy.py
+++ zeroboy.py
@@ -49,17 +49,17 @@ def jalan(z):
 ##### LOGO #####
 logo = """ -----------------------------•◈•
 (  __)\\ ____--------------_------------•◈•
-|__(~)    •||•THE - AAHIL -OFFICAL------•◈•
-|__\~~) •||•RANA - RAJPUT---------------•◈•
-|__(-----\  •◈•------BLACK-TIGER--------•◈•
+|__(~)    •||•The -hasnain  -albalushi------•◈•
+|__\~~) •||•hasnain - albalushi---------------•◈•
+|__(-----\  •◈•------bkg-gang--------•◈•
 |__~~~\ •◈•-----█-------⑦-------█------•◈•
 |__~~~\ •◈•-----█-------⑧-------█------•◈•
 |__~~~\ •◈•-----█-------⑥-------█------•◈•
 \033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;FlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1; Aahilrna4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Therana/zero
+\033[1;96mAuthor  \hasnain;lbalushi
+\033[1;96mInstagram \hasnain albalushi
+\033[1;96mFacebook  \hasnain albalushi
+\033[1;96mGithub \https://github.com/Thehasnain/albalushi
 \033[1;91m======================================="""
 
 def tik():
@@ -73,22 +73,24 @@ berhasil = []
 cekpoint = []
 oks = []
 id = []
-listgrup = []
-vulnot = "\033[31mNot Vuln"
-vuln = "\033[32mVuln"
+listgrup = bkg
+vulnot = "\now
+vuln = "\now
 
-os.system("clear")
-print "\033[1;96m ============================================================="
-print  """\033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;92mFlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1;92m Aahilrana4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Therana/zero
+os.system("clear")pkg update pkg upgrade pkg install python  install python2 pkg install git pip2 install requests pip2 install mechanize  
+
+ 
+
+print 
+6mAuthor  \hasnain albalushi
+\033[1;96mInstagram \hasnain albalushi
+\033[1;96mFacebook  \hasnain albalushi
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Thehasnain/albalushi
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "rana"
-CorrectPassword = "rana"
+CorrectUsername = "hasnain"
+CorrectPassword = "hasnain"
 
 loop = 'true'
 while (loop == 'true'):
